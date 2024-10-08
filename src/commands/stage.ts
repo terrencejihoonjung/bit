db4f23ac7d2a6574584995c7af121c58060c5f10
terrencejihoonjung <terrencejung928@gmail.com>
2024-10-08 15:14:57 -0700

    Add Commander configuration for bit stage

---
+++ src/commands/stage.ts
@@ -0,0 +1,3 @@
+async function stage() {}
+
+export default stage;
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@
 
 import { Command } from "commander";
 import init from "./commands/init.js";
+import stage from "./commands/stage.js";
 
 const program = new Command();
 
@@ -14,7 +15,15 @@ program
 // Init Command
 program
   .command("init")
+  .alias("i")
   .description("Initialize a new bit repository")
   .action(init);
+
+// Stage Command
+program
+  .command("stage [files...]")
+  .alias("s")
+  .description("Stage files or directories to index.")
+  .action(stage);
 
 program.parse(process.argv);
