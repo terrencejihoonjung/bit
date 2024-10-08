0f4d304849ad2044ecbfe0a4dbcdcb6663e36264
terrencejihoonjung <terrencejung928@gmail.com>
2024-10-08 15:16:47 -0700

    Set up test file for bit stage

---
+++ tests/unit/commands/stage.test.ts
@@ -0,0 +1,7 @@
+import stage from "../../../src/commands/stage";
+
+describe("bit stage [files...]", () => {
+  it("should stage all files given files/directories", () => {});
+
+  it("should not stage files already staged", () => {});
+});
